510d7237e03703c4dd9aec81e2f369978a2e41b7
zhy <zhy@gmail.com>
2020-04-10 07:13:54 +0800

    tt

--- dist/index.js
+++ dist/index.js
@@ -977,15 +977,15 @@ function run() {
         try {
             const ms = core.getInput('milliseconds');
             core.debug(`Waiting ${ms} milliseconds ...`);
-            core.info("ms = ${ms}");
+            core.info(`ms = ${ms}`);
             //core.debug(new Date().toTimeString())
             //await wait(parseInt(ms, 10))
             //core.debug(new Date().toTimeString())
             //console.log(process.env)
             var OS = process.env.RUNNER_OS;
-            core.info("OS = ${OS}");
+            core.info(`OS = ${OS}`);
             var GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
-            core.info("GITHUB_REPOSITORY = ${GITHUB_REPOSITORY}");
+            core.info(`GITHUB_REPOSITORY = ${GITHUB_REPOSITORY}`);
             if (OS == 'windows') {
             }
             else {
