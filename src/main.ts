63884cef590590860ce96d609d9ed7a7acac0698
zhy <zhy@gmail.com>
2020-04-09 23:50:10 +0800

    tt

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from "@actions/exec";
+import * as core from "@actions/core";
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
@@ -10,10 +11,31 @@ async function run(): Promise<void> {
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
+    core.info(await getStdout("uname",["-a"]))
+
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+async function getStdout(
+  exe: string,
+  args: string[],
+  options?: {}
+): Promise<string> {
+  let stdout = "";
+  const resOptions = Object.assign({}, options, {
+    listeners: {
+      stdout: (buffer: Buffer): void => {
+        stdout += buffer.toString();
+      },
+    },
+  });
+
+  await exec.exec(exe, args, resOptions);
+
+  return stdout;
 }
 
 run()
